f2d88d662008a21a6eeb92be3be87833ed0faa82
Nicholi <nmcaron@protonmail.ch>
2022-06-30 18:40:16 -0400

    Updated backend/src/main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,19 @@
-use std::io::{Error,ErrorKind};
-use std::str::FromStr;
-use serde::Serialize;
+// GO vs RUST for web api's
+//
+// Pro Rust: 
+//      - Data processing capabilities
+//      - Error handling
+//      - generics
+//      - Closure serialization (makes 'moving compute to data' [hadoop/spark] easier)
+//      - Clean interface to C (not sure if this will be relevant to this project)
+//      - 
+//
+// Pro Go (GoPro? lol):
+//      - Far more ergonomic for this sorta thing
+//      - Better libraries
+
 use warp::{
+use serde::{Deserialize, Serialize};
     Filter,
     filters::{
         cors::CorsForbidden
@@ -15,6 +27,7 @@ use warp::{
 use std::collections::HashMap;
 
 // local store -- to later be replaced by a DB
+#[derive(Clone)]
 struct Store {
     // Using a hashmap here so that we can index an item given its ID w/o traversing the whole
     // collection
@@ -27,10 +40,11 @@ impl Store {
             posts: HashMap::new(),
         }
     }
-    fn init() {}
-    fn add_post(mut self, post: Post) -> Self {
-        self.posts.insert(post.id.clone(), post);
-        self
+    fn init() -> HashMap<PostID, Post> {
+        // REMEMBER TO USE SOMETHING OTHER THAN FROM_STR FOR ARTIST_DATA STRUCTS
+        // JSON Files will be way too large
+        let file = include_str!("../posts.json");
+        serde_json::from_str(file).expect("Can't read posts.json file")
     }
 }
 
@@ -44,22 +58,13 @@ struct Post {
     tags: Option<Vec<String>>,
 }
 
-impl Post {
-    fn new(id: PostID, author: Username, title: String, content: String, tags: Option<Vec<String>>) -> Post {
-        Post {
-            id,
-            author,
-            title,
-            content,
-            tags,
-        }
-    }
+// impl Post {
     // Instead of instantiating a new question instance, we could edit the existing instance,
     // but then we wade into the "lifetime" waters
-    fn update_title(&self, new_title: String) -> Post {
-        Post::new(self.id.clone(), self.author.clone(), new_title, self.content.clone(), self.tags.clone())
-    }
-}
+    // fn update_title(&self, new_title: String) -> Post {
+    //    Post::new(self.id.clone(), self.author.clone(), new_title, self.content.clone(), self.tags.clone())
+    //}
+//}
 
 // Newtype Idiom differentiates QuestionID types from normal strings
 // https://doc.rust-lang.org/rust-by-example/generics/new_types.html
@@ -69,31 +74,12 @@ struct PostID(String);
 #[derive(Debug, Clone, Serialize)]
 struct Username(String);
 
-// Implement FromStr to allow easier creation of QuestionID (via a string slice)
-impl FromStr for PostID {
-    type Err = std::io::Error;
-    fn from_str(id: &str) -> Result<Self, Self::Err> {
-        match id.is_empty() {
-            false => Ok(PostID(id.to_string())),
-            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
-        }
-    }
-}
-// Custom warp reject type
-#[derive(Debug)]
-struct InvalidID;
-impl Reject for InvalidID {}
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
     // r.find() allows us to search for specific rejections
     if let Some(error) = r.find::<CorsForbidden>() {
         Ok(warp::reply::with_status(
             error.to_string(),
-            StatusCode::UNPROCESSABLE_ENTITY,
-        ))
-    } else if let Some(_invalid_id) = r.find::<InvalidID>() {
-        Ok(warp::reply::with_status(
-            "No valid ID presented".to_string(),
             StatusCode::UNPROCESSABLE_ENTITY,
         ))
     } else {
@@ -106,28 +92,22 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
 
 // First route handler, returns either a reply or rejection
 async fn get_post() -> Result<impl Reply, Rejection> {
-    let post = Post::new(
-        PostID::from_str("1").expect("No id provided"),
-        Username("Anthony Fantano".to_string()),
-        "First Question".to_string(),
-        "Content of Question".to_string(),
-        Some(vec!("faq".to_string())),
-   );
-
-    match post.id.0.parse::<i32>() {
-        Err(_) => {
-            Err(warp::reject::custom(InvalidID))
-        },
-        Ok(_) => 
-            Ok(warp::reply::json(
-                &post
-            ))
-    }
+    // .values() & .cloned()  methods from hashmap??? Check docs
+    let res: Vec<Post> = store.questions.values().cloned().collect()
+        Ok(warp::reply::json(&res))
 }
 
 #[tokio::main]
 async fn main() {
+    let store = Store::new();
+    // The any filter matches any request, so this statement evaluates for any and all requests
+    // Map (w/ move closure) passes the store by value (following cloning) into the filter  so that
+    // each route handler has access to the store 
+    let store_filter = warp::any().map(move || store.clone());
+
     // Cross-Origin Resource Sharing (https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS)
+    // "an HTTP-header based mechanism that allows a server to indicate any origins other than its
+    // own from which a browser should permit loading resources"
     let cors = warp::cors()
         .allow_any_origin()
         .allow_header("not-in-the-request")
@@ -136,10 +116,15 @@ async fn main() {
         .allow_method(&Method::GET)
         .allow_method(&Method::POST);
 
+    // What is a filter?
+    // Each HTTP requeset runs through the filters we setup and adds or modifies the data along the
+    // way
+    //
     // create a path Filter, chaining several filters
     let get_items = warp::get()
         .and(warp::path("posts"))
         .and(warp::path::end())
+        .and(store_filter)
         .and_then(get_post)
         // error handling filter, fetches every prev rejection and check
         // which HTTP message we need to send back
